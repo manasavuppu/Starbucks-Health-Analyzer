3af73e3665e4fb3b77d506e7af5402a6f699aafa
geethika992 <geethikabalasubramanian92@gmail.com>
2023-12-08 12:17:14 -0500

    Removed Comments and formatted

--- Introduction.py
+++ Introduction.py
@@ -1,114 +1,167 @@
 import streamlit as st
 import pandas as pd
-import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 import pycountry
 import pydeck as pdk
-from urllib.request import urlopen
-import json
 import plotly.express as px
+import time
 st.set_page_config(layout="wide")
+col1, col2 = st.columns((0.3, 2))
+with col1:
+    image = 'Images/starbucks_logo.png'
+    st.image(image, width=100)
+with col2:
+    st.write("# Sip Smart: Healthy Choices at Starbucks.")
+    st.write("Explore the nutritional landscape of your Starbucks drinks—uncover insights and make informed choices for a healthier sip.")
+tab1, tab2, tab3 = st.tabs(["Introduction", "Background", "About the App"])
 
-st.write("# Let's start with a warm cup of Coffee! ")
-tab1, tab2 = st.tabs(["Introduction", "About this App"])
-
-with tab1: 
-        st.markdown(
-                """
-            As Starbucks has emerged as a global coffee giant, its largest market, the **United States**, has faced a growing concern - the challenge of obesity. As Starbucks continues to expand its reach worldwide, the nutritional implications of its offerings, 
-            particularly in a country where it holds significant market dominance, become a critical point of examination.
-            You can choose different countries to compare
+with tab1:
+    st.markdown(
+        """
+           As **Starbucks**, a global coffee powerhouse, expands its footprint across the globe, understanding the nutritional landscape of its offerings becomes increasingly crucial. Particularly in the **United States**, where Starbucks holds significant market dominance, this app allows you to explore and compare nutritional information, empowering you to make informed choices.
             """
-            )
+    )
 
-        st.markdown(
-                """
-            Zoom out to find the concentration of stores across the world.
-            """
-            )
-        @st.cache_data  
-        def load_data(file):
-            df_map = pd.read_csv(file)
-            return df_map
-        df_map = load_data('directory.csv')
-        df_map_cleaned = df_map.dropna(subset=['Latitude', 'Longitude'])
 
-        df_country_bar=df_map_cleaned.groupby('Country')['Country'].count().reset_index(name='Count')
-        df_country_bar_sorted = df_country_bar.sort_values(by='Count', ascending=False)
-        df_country_bar_sorted['CountryName'] = df_country_bar_sorted['Country'].apply(lambda code: pycountry.countries.get(alpha_2=code).name)
-        df_country_bar_sorted['CountryName'] = df_country_bar_sorted['CountryName'].replace('Korea, Republic of', 'South Korea')
+# Cache data for faster Data Retrieval
 
-        Country_list=df_country_bar_sorted['CountryName'].unique().tolist()
-        Country_list_top5=df_country_bar_sorted['CountryName'].head(5).tolist()
-        options = st.multiselect(
-                'Select the Countries for comparison',Country_list,default=Country_list_top5
-                )
+    @st.cache_data
+    def load_data(file):
+        df_map = pd.read_csv(file)
+        return df_map
+    df_map = load_data('Data/directory.csv')
+    df_nutri = load_data('Data/avg_nutri.csv')
+    df_map_cleaned = df_map.dropna(subset=['Latitude', 'Longitude'])
 
-        df_country_bar_sorted_filtered = df_country_bar_sorted[df_country_bar_sorted['CountryName'].isin(options)]
-        max_count = df_country_bar_sorted_filtered['Count'].max()
-        chart = alt.Chart(df_country_bar_sorted_filtered).mark_bar(color='#006241').encode(
-                x=alt.X('Count:Q',title="Number of Stores",scale=alt.Scale(domain=(0, max_count+900))),
-                y=alt.Y('CountryName:N', title="Country", sort=alt.EncodingSortField(field='Count', op='sum', order='descending')),
-                tooltip=[alt.Tooltip('CountryName:O', title="Country Name"), alt.Tooltip('Count:Q', title="Number of Stores")]
-            ).properties(
-                title='Countries with Most Starbucks Stores',
-                width=500,
-                height=500
-            )
-        text = chart.mark_text(
-                align='center',
-                baseline='middle',
-                dx=12,  
-                
-                color='Black'  # Text color
-            ).encode(
-                text='Count:Q'  
-            )
+    df_country_bar = df_map_cleaned.groupby(
+        'Country')['Country'].count().reset_index(name='Count')
+    df_country_bar_sorted = df_country_bar.sort_values(by='Count', ascending=False)
+    # Add Country name from pycountry based on the country code
+    df_country_bar_sorted['CountryName'] = df_country_bar_sorted['Country'].apply(
+        lambda code: pycountry.countries.get(alpha_2=code).name)
+    df_country_bar_sorted['CountryName'] = df_country_bar_sorted['CountryName'].replace(
+        'Korea, Republic of', 'South Korea')
+    st.markdown(
+        """**Add your Favourite countries to see the count of Starbucks stores**""")
+    Country_list = df_country_bar_sorted['CountryName'].unique().tolist()
+    Country_list_top5 = df_country_bar_sorted['CountryName'].head(
+        5).tolist()  # Top 5 countries with high store count
+    # Country select box creation
+    options = st.multiselect(
+        'Choose your countries from the drop down', Country_list, default=Country_list_top5
+    )
 
-            # Combine the bar chart and text layer
-        chart_with_text = (chart + text).interactive()
+    df_country_bar_sorted_filtered = df_country_bar_sorted[df_country_bar_sorted['CountryName'].isin(
+        options)]
+    max_count = df_country_bar_sorted_filtered['Count'].max()
+    st.markdown(
+        """Zoom out and explore the global concentration of Starbucks stores."""
+    )
+   # Bar chart for store density analysis
+    chart = alt.Chart(df_country_bar_sorted_filtered).mark_bar(color='#006241').encode(
+        x=alt.X('Count:Q', title="Number of Stores",
+                scale=alt.Scale(domain=(0, max_count+900))),
+        y=alt.Y('CountryName:N', title="Country", sort=alt.EncodingSortField(
+            field='Count', op='sum', order='descending')),
+        tooltip=[alt.Tooltip('CountryName:O', title="Country Name"), alt.Tooltip(
+            'Count:Q', title="Number of Stores")]
+    ).properties(
+        title='Starbucks Store Count',
+        width=500,
+        height=550
+    )
+    text = chart.mark_text(
+        align='center',
+        baseline='middle',
+        dx=12,
 
-        col1, col2 = st.columns((2,2))
-        with col1:
-            st.pydeck_chart(pdk.Deck(
+        color='Black'  # Text color
+    ).encode(
+        text='Count:Q'
+    )
+
+    # Combine the bar chart and text layer
+    chart_with_text = (chart + text).interactive()
+
+    col1, col2 = st.columns((2, 2))
+    with col1:
+        with st.spinner('Wait for it...'):
+            time.sleep(3)
+        # Map creation
+        st.pydeck_chart(pdk.Deck(
             map_style=None,
             initial_view_state=pdk.ViewState(
                 latitude=31.76,
                 longitude=-97.4,
                 zoom=3,
                 pitch=0,
-                
+
             ),
             layers=[
-                
+
                 pdk.Layer(
                     'ScatterplotLayer',
                     data=df_map_cleaned,
                     get_position='[Longitude, Latitude]',
-                    #get_color='[200, 2,2]',
                     get_radius=10000,
                     get_fill_color=[0, 98, 65],
                     get_line_color=[0, 0, 0],
-                    #tooltip="City: {City}"
                 ),
             ]
         ))
-            
 
-        
-        with col2:
-                st.altair_chart(chart_with_text, use_container_width=True)
+    with col2:
+        if df_country_bar_sorted_filtered.empty:
+            st.write("Choose your countries from the list.")
+        else:
+            st.altair_chart(chart_with_text, use_container_width=True)
 
 with tab2:
-        
-        st.markdown(
-            """
-        As Starbucks has emerged as a global coffee giant, its largest market, the **United States**, has faced a growing concern - the challenge of obesity. As Starbucks continues to expand its reach worldwide, the nutritional implications of its offerings, 
-        particularly in a country where it holds significant market dominance, become a critical point of examination.
-        You can choose different countries to compare
-        """
-        )
 
+    st.markdown("""
+         **The percent Daily Value (% DV) tells how much a nutrient in a serving of the food or beverage contributes to a total daily 2,000-calorie diet.**""")
+    st.markdown("""As per US Food and Drug Administration (FDA) **20% Daily Value or more of a nutrient per serving is considered high**""")
+
+    st.markdown("""Let's identify the primary nutrient that significantly influences the %Daily Value in Starbucks beverages, on an average""")
+    df_nutri['perc_values'] = df_nutri['Value'].round(2).astype(str)+'%'
+    bars = alt.Chart(df_nutri).mark_bar().encode(
+        x='Metric',
+        y='Value',
+        tooltip=[alt.Tooltip('Metric:O', title="Nutrient"),
+                 alt.Tooltip('Value:Q', title="%Daily value")],
+        color=alt.Color('Value', scale=alt.Scale(
+            scheme='browns'), legend=None),
+    ).properties(height=alt.Step(70), width=alt.Step(80))
+
+    # Text labels
+    text = alt.Chart(df_nutri).mark_text(align='center', baseline='middle', dy=-5, color='black').encode(
+        x=alt.X('Metric:N', title='Nutrient'),
+        y=alt.Y('Value:Q', title='%Daily Value'),
+        text=alt.Text('perc_values:N'),
+
+    )
+
+    combined = (bars+text)
+    st.altair_chart(combined, use_container_width=True)
+    st.markdown("""Sugar and caffeine emerge as prominent contributors to the %Daily Value with **Sugar at 68.64%** and **Caffeine reaching 20%**.""")
+
+with tab3:
+    st.markdown("""This app methodically compares Starbucks drinks, assessing Sugar and Caffeine content, and considering Percentage Daily Value. Users can select beverage categories and protein ranges, finding a balance between indulgence and nutrition. Drinks are categorized as **Balanced Brews** and **'Over-indulgent** choices.""")
+    st.markdown("""Dive deeper into individual beverages with the app's insightful feature. Uncover the **Nutri Score** based on Sugar and Caffeine, allowing you to categorize healthiness effortlessly with an interactive **categorization meter**.""")
+
+    st.subheader("Data Sources:")
+    BNV = '[Starbucks Nutritional content](https://stories.starbucks.com/uploads/2019/01/nutrition-1.pdf)'
+    SSC = '[Starbucks Stores](https://data.world/data-hut/starbucks-location-dataset)'
+
+    st.markdown(BNV, unsafe_allow_html=True)
+    st.markdown(SSC, unsafe_allow_html=True)
+
+    st.subheader('Image Sources:')
+    st.markdown('https://creative.starbucks.com/logos/')
+    st.subheader('References:')
+    FDA = '[US Food and Drug Administration](https://www.fda.gov/food/nutrition-facts-label/daily-value-nutrition-and-supplement-facts-labels)'
+    st.markdown(FDA, unsafe_allow_html=True)
+    NS = '[Nutri-Score]( https://en.wikipedia.org/wiki/Nutri-Score)'
+    st.markdown(NS, unsafe_allow_html=True)
---
+++ pages/How_healthy_is_your_Starbucks_Beverage.py
@@ -0,0 +1,263 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+import pydeck as pdk
+import datetime as dt
+import seaborn as sns
+import plotly.graph_objects as go
+st.set_page_config(layout='wide')
+image = 'Images/Starbucks_logo.png'
+st.image(image, width=80)
+
+# Cache data for faster data Retreival
+
+
+@st.cache_data
+def load_data(file):
+    df = pd.read_csv(file)
+    return df
+
+
+df = load_data('Data/starbucks_nutrition.csv')
+dv = df.copy(deep=True)
+# %Daily Value Computations
+dv['Calories'] = dv['Calories'] / 2000 * 100
+dv['Total Fat'] = dv['Total Fat (g)'] / 78 * 100
+dv['Cholesterol'] = dv['Cholesterol (mg)'] / 300 * 100
+dv['Carbohydrates'] = dv['Total Carbohydrates (g)'] / 275 * 100
+dv['Sugar'] = dv['Sugar (g)'] / 50 * 100
+dv['Protein'] = dv['Protein (g)'] / 50 * 100
+dv['Caffeine'] = dv['Caffeine (mg)'] / 400 * 100
+dv['Sugar(%DV)'] = dv['Sugar'].round(2).astype(str)+'%'
+dv['Caffeine(%DV)'] = dv['Caffeine'].round(2).astype(str)+'%'
+
+
+# Functions for Data Processing
+def determine_healthiness(row):
+    if row['Sugar'] > 20 or row['Caffeine'] > 20:
+        return 'Over-indulgent'
+    else:
+        return 'Balanced Brews'
+
+
+# Apply the function to create the 'Healthiness' column
+dv['Healthiness'] = dv.apply(determine_healthiness, axis=1)
+
+
+def calculate_nutri_score(row):
+    # Nutritional factors and their weights
+    factors = {
+        "Sugar": {"weight": 0.55, "base": 0.0},
+        "Caffeine": {"weight": 0.5, "base": 0.0},  # Custom weight for caffeine
+    }
+
+    # Calculate scores for each factor
+    scores = {factor: max(0, factors[factor]["weight"] * (
+        row[factor] - factors[factor]["base"])) for factor in factors}
+
+    # Calculate the overall Nutri-Score
+    nutri_score = sum(scores.values())
+
+    return nutri_score
+
+
+dv["Nutri Score"] = dv.apply(calculate_nutri_score, axis=1)
+
+# Sidebar Selection Parameters
+# Baverage Category selectbox
+
+bev_cat_options = ['All Categories']
+bev_cat_options += dv['Beverage Category'].unique().tolist()
+sel_bev_cat = st.sidebar.selectbox(
+    "Choose a category to analyze", bev_cat_options, placeholder='All Categories')
+if sel_bev_cat == 'All Categories':
+    sel_bev_cat = dv['Beverage Category'].unique().tolist()
+    df_bev_cat_fil = dv[dv['Beverage Category'].isin(sel_bev_cat)]
+else:
+    df_bev_cat_fil = dv[dv['Beverage Category'].isin([sel_bev_cat])]
+
+df_no_duplicates = df_bev_cat_fil.drop_duplicates(subset='Beverage')
+
+# Protien Slider
+select_protien = st.sidebar.slider(
+    "Select a range for Protien(g)", min_value=0, max_value=40, value=(0, 20))
+df_with_protein_fil = df_no_duplicates[(df_no_duplicates['Protein (g)'] >= select_protien[0]) & (
+    df_no_duplicates['Protein (g)'] <= select_protien[1])]
+df_with_protein_fil['Sugar(%DV)'] = df_with_protein_fil['Sugar'].round(
+    2).astype(str)+'%'
+df_with_protein_fil['Caffeine(%DV)'] = df_with_protein_fil['Caffeine'].round(
+    2).astype(str)+'%'
+
+
+# Tab Creation
+tab1, tab2 = st.tabs(["Beverage Category", "Beverages"])
+with tab1:
+    st.markdown(
+        """
+           **Select a Beverage category and choose a Protien range from the sidebar to explore how beverages are distributed in the Sugar (%DV) vs Caffeine (%DV) space.** 
+           \n Hover over the dots for details. Scroll down for Beverage Recommendations
+             """
+    )
+# altair scatter plot
+    colors = ['#006241', '#b90000']
+    chart = alt.Chart(df_with_protein_fil).mark_circle(size=90).encode(
+        x=alt.X('Sugar:Q', scale=alt.Scale(
+            domain=(-5, 200)), title='Sugar(%Dv)'),
+        y=alt.Y('Caffeine:Q', scale=alt.Scale(
+            domain=(-5, 50)), title='Caffeine(%Dv)'),
+        color=alt.Color('Healthiness', scale=alt.Scale(
+            domain=['Balanced Brews', 'Over-indulgent'], range=colors)),
+        tooltip=[
+            alt.Tooltip('Beverage:N', title='Beverage Name'),
+            alt.Tooltip('Sugar(%DV)', title='Sugar (%DV)'),
+            alt.Tooltip('Caffeine(%DV)', title='Caffeine (%DV)')
+        ]
+    ).properties(height=500, width=800).interactive()
+
+    # Reference Line x
+    reference_line_x = alt.Chart(pd.DataFrame(([{'Caffeine': 20}]))).mark_rule(strokeDash=[3, 3], color='gray').encode(
+        y='Caffeine:Q'
+    )
+    # Reference Line y
+    reference_line_y = alt.Chart(pd.DataFrame(([{'Sugar': 20}]))).mark_rule(strokeDash=[3, 3], color='gray').encode(
+        x='Sugar:Q'
+    )
+
+    combined = (chart+reference_line_x+reference_line_y).interactive()
+    st.altair_chart(combined, use_container_width=True)
+# Recommendations creation
+    df_with_protein_fil_recommended = df_with_protein_fil[
+        df_with_protein_fil['Healthiness'] == 'Balanced Brews']
+    if df_with_protein_fil_recommended.empty:
+        st.success(
+            "We do not have any Recommendations catering to your Choices. However, Below are few healthier options you can choose from!")
+
+        recommended_df = dv.query(
+            "Healthiness == 'Balanced Brews'").sample(n=5)
+        styled_df = recommended_df[['Beverage', 'Milk', 'Size']]
+        styled_df['Beverage'] = styled_df['Beverage'].apply(
+            lambda x: f'<span style="color:green">{x}</span>')
+
+        st.write(styled_df.to_html(escape=False, index=False),
+                 unsafe_allow_html=True, hide_index=True)
+
+    else:
+
+        st.success("Here are our recommendations from Balanced brews!")
+        styled_df = df_with_protein_fil_recommended[[
+            'Beverage', 'Milk', 'Size']].head(5)
+        styled_df['Beverage'] = styled_df['Beverage'].apply(
+            lambda x: f'<span style="color:green">{x}</span>')
+        st.write(styled_df.to_html(escape=False, index=False),
+                 unsafe_allow_html=True, hide_index=True)
+
+    st.markdown(
+        """
+                    Would you like to know if your favourite Beverage is a healthy option? **Go to Beverages tab.**
+             """
+    )
+
+
+with tab2:
+    # Bevereges list
+    bev_options = df_with_protein_fil['Beverage'].unique().tolist()
+    sel_bev = st.selectbox(
+        "Choose a Starbucks Beverage from the Selected Category ", bev_options)
+    df_bev_fil = df_with_protein_fil[df_with_protein_fil['Beverage'].isin([
+                                                                          sel_bev])]
+    col1, col2, col3 = st.columns((4, 0.25, 2.5))
+    with col1:
+        # Nutriscore meter creation
+        current_value = df_bev_fil['Nutri Score'].mean()
+
+        plot_bgcolor = "#def"
+        quadrant_colors = [plot_bgcolor, "#b22b27", "#e69138", "#006241"]
+        quadrant_text = ["", "<b>Unbalanced Delights</b>",
+                         "<b>Moderated Indulgences</b>", "<b>Balanced Option</b>"]
+        n_quadrants = len(quadrant_colors) - 1
+
+        current_value = df_bev_fil['Nutri Score'].mean()
+        min_value = 0
+        max_value = 50
+        hand_length = np.sqrt(2) / 4
+        hand_angle = np.pi * \
+            (1 - (max(min_value, min(max_value, current_value)) -
+             min_value) / (max_value - min_value))
+
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    values=[0.5] + (np.ones(n_quadrants) /
+                                    2 / n_quadrants).tolist(),
+                    rotation=90,
+                    hole=0.5,
+                    marker_colors=quadrant_colors,
+                    text=quadrant_text,
+                    textinfo="text",
+                    hoverinfo="skip",
+                ),
+            ],
+            layout=go.Layout(
+                showlegend=False,
+                margin=dict(b=0, t=10, l=10, r=10),
+                width=500,
+                height=500,
+                paper_bgcolor=plot_bgcolor,
+                annotations=[
+                    go.layout.Annotation(
+                        text=f"<b>Nutri Score:</b><br>{round(current_value,2)}",
+                        x=0.5, xanchor="center", xref="paper",
+                        y=0.25, yanchor="bottom", yref="paper",
+                        showarrow=False,
+                        font=dict(color="black", size=16)
+                    )
+                ],
+                shapes=[
+                    go.layout.Shape(
+                        type="circle",
+                        x0=0.48, x1=0.52,
+                        y0=0.48, y1=0.52,
+                        fillcolor="#333",
+                        line_color="#333",
+                    ),
+                    go.layout.Shape(
+                        type="line",
+                        x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
+                        y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
+                        line=dict(color="#333", width=4)
+                    )
+                ]
+            )
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+    with col3:
+        # Altair bar chart creation
+        df_avg = df_bev_fil.groupby(['Beverage'])[
+            ['Sugar', 'Caffeine']].mean().reset_index()
+        df_long = pd.melt(df_avg[['Beverage', 'Sugar', 'Caffeine']], id_vars=[
+                          'Beverage'], var_name='Nutrient', value_name='Score')
+
+        df_long['Score'] = df_long['Score'].round(2)
+        df_long['Score_perc'] = df_long['Score'].astype(str)+'%'
+
+        bars1 = alt.Chart(df_long).mark_bar().encode(
+            y=alt.Y('Score:Q', scale=alt.Scale(
+                domain=(0, 100)), title='%Daily Value'),
+            x='Nutrient:N',
+            color=alt.Color('Score:Q', scale=alt.Scale(
+                scheme='browns'), legend=None),
+            tooltip=[
+                alt.Tooltip('Score', title='%Daily Value:')
+            ]
+        ).properties(height=500, width=150)
+        text = alt.Chart(df_long).mark_text(dy=-5, dx=3, color='black').encode(
+            y=alt.Y('Score:Q'),
+            x=alt.X('Nutrient:N'),
+            text=alt.Text('Score_perc:N')
+
+        )
+
+        st.altair_chart(bars1+text, use_container_width=True)
